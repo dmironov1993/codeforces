61d713c424d6ec1cf131381f46f2bb110c8da471
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-11 15:37:30 +0300

    Create 581A-VasyatheHipster.py

---
+++ 581A-VasyatheHipster.py
@@ -0,0 +1,12 @@
+# https://codeforces.com/contest/581/submission/52599521
+
+a,b = tuple(map(int, input().split()))
+
+ans = []
+if a > b:
+    ans.append(b)
+    ans.append((a-b)//2)
+else:
+    ans.append(a)
+    ans.append((b-a)//2)
+print (*ans)
