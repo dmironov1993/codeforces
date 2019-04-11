e77ffc87022443a7ec83ad5fbad7e7d30afc164a
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-11 15:36:19 +0300

    Create 758A-HolidayOfEquality.py

---
+++ 758A-HolidayOfEquality.py
@@ -0,0 +1,15 @@
+# https://codeforces.com/contest/758/submission/52599738
+
+n = int(input())
+a = list(map(int, input().split()))
+
+S = 0
+if len(a) == 1:
+    S = 0
+else:
+    max_a = max(a)
+    for i in range(n):
+        diff = max_a - a[i]
+        if diff > 0:
+            S += diff
+print (S)
