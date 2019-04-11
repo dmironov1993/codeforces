94ba7e16fc4c7d7f4391c99e8bb07bee1a7c14ae
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-11 15:39:58 +0300

    Create 959A-MahmoudandEhabandtheeven-odd game.py

---
+++ 959A-MahmoudandEhabandtheeven-odd game.py
@@ -0,0 +1,20 @@
+# https://codeforces.com/contest/959/submission/52599072
+
+n = int(input())
+
+turn = 'Mahmoud'
+while True:
+    if n % 2 == 0 and turn == 'Mahmoud':
+        n -= n%2
+        turn = 'Ehab'
+    elif n % 2 !=0 and turn == 'Ehab':
+        n -= n%2
+        turn = 'Mahmoud'
+    else:
+        if turn == 'Mahmoud':
+            turn = 'Ehab'
+        else:
+            turn = 'Mahmoud'
+        break
+        
+print (turn)
