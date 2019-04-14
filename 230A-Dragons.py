de71d65436c4ede996c47ab7326e39ac191c4e63
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-14 23:28:00 +0300

    Create 230A-Dragons.py

---
+++ 230A-Dragons.py
@@ -0,0 +1,15 @@
+# https://codeforces.com/contest/230/problem/A
+# https://codeforces.com/contest/230/submission/52564798
+
+s,n = tuple(map(int, input().split()))
+
+game_sets = sorted([tuple(map(int, input().split())) for _ in range(n)])
+
+outcome = 'YES'
+for i in range(n):
+    if s > game_sets[i][0]:
+        s += game_sets[i][1]
+    else:
+        outcome = 'NO'
+        break
+print (outcome)
