9ae855c4533e750d120390153d24c0d746a62bd6
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-14 23:24:51 +0300

    Create 1132B-Discounts.py

---
+++ 1132B-Discounts.py
@@ -0,0 +1,11 @@
+# https://codeforces.com/contest/1132/problem/B
+# https://codeforces.com/contest/1132/submission/52562005
+
+n = int(input()) # number of bars
+a = sorted(list(map(int, input().split())), reverse=True)  # cost of a bar
+m = int(input())  # number of coupons
+q = list(map(int, input().split()))  # 
+summa = sum(a)
+
+for el in q:
+    print (summa - a[el-1])
