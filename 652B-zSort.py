a78bec52e27dea435b11eac32aef84ba3e015e01
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-14 23:29:05 +0300

    Create 652B-zSort.py

---
+++ 652B-zSort.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/contest/652/problem/B
+# https://codeforces.com/contest/652/submission/52528822
+
+
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+odd = [arr[i] for i in range(1,n,2)]
+even = [arr[i] for i in range(0,n,2)]
+
+ans = []
+for i in range(1,n+1):
+    if i % 2 != 0:
+        ans.append(arr[0])
+        arr.pop(0)
+    else:
+        ans.append(arr[-1])
+        arr.pop(-1)
+print (*ans)
