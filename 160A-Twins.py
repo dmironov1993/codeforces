5e888b93a791fdff94433699a0ff5c4ced2f0357
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-14 23:27:06 +0300

    Create 160A-Twins.py

---
+++ 160A-Twins.py
@@ -0,0 +1,17 @@
+# https://codeforces.com/contest/160/problem/A
+# https://codeforces.com/contest/160/submission/52564033
+
+n = int(input())
+a = sorted(list(map(int, input().split())), reverse=True)
+
+count_me = 0
+summa = 0
+condition_1 = 0
+condition_2 = 0
+for i in range(n):
+    count_me += 1
+    summa += a[i]
+    if sum(a[i+1:]) < summa:
+        break
+        
+print (count_me)
