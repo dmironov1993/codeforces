dc059a556446b9598c72359496fdaaaf94a42a70
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-15 15:16:33 +0300

    Create 785A-Anton and Polyhedrons.py

---
+++ 785A-Anton and Polyhedrons.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/problemset/problem/785/A
+# https://codeforces.com/contest/785/submission/52776494
+
+dct = {'Tetrahedron':4,
+       'Cube':6,
+       'Octahedron':8,
+       'Dodecahedron':12,
+       'Icosahedron':20}
+
+n = int(input())
+s = []
+count = 0
+for _ in range(n):
+    s.append(input())
+for i in range(n):
+    name = s[i]
+    count += dct[name]
+print(count)
