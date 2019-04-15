1394049e0d6e191ea761df729b81074de4bbf8fa
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-15 15:09:43 +0300

    Create 705A-Hulk.py

---
+++ 705A-Hulk.py
@@ -0,0 +1,20 @@
+# https://codeforces.com/problemset/problem/705/A
+# https://codeforces.com/contest/705/submission/52776240
+
+first = 'I hate'
+second = 'I love'
+sentence = 'I hate'
+turn = 0
+n = int(input())
+if n == 1:
+    print (sentence + ' it')
+else:
+    for _ in range(n-1):
+        if turn == 0:
+            turn = 1
+            sentence += ' that ' + second
+        else:
+            turn = 0
+            sentence += ' that ' + first
+    sentence += ' it'
+    print (sentence)
