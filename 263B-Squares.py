c606c4766440e152074474fe54c1b0d1701dfda4
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-15 16:35:50 +0300

    Update 263B-Squares.py

--- 263B-Squares.py
+++ 263B-Squares.py
@@ -1,6 +1,4 @@
 # https://codeforces.com/contest/263/problem/B
-# https://codeforces.com/contest/263/submission/52577964
-
 
 n,k = tuple(map(int, input().split()))
 a = sorted(list(map(int, input().split())))
