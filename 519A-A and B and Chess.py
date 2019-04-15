537f01027b242c9f8b309521b3f41d41dd0a581b
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-15 16:32:00 +0300

    Create 519A-A and B and Chess.py

---
+++ 519A-A and B and Chess.py
@@ -0,0 +1,38 @@
+# https://codeforces.com/problemset/problem/519/A
+# https://codeforces.com/contest/519/submission/52779416
+
+# white
+dct_w = {'Q':9,
+       'R':5,
+       'B':3,
+       'N':3,
+       'P':1,
+       'K':0}
+# black
+dct_b = {'q':9,
+       'r':5,
+       'b':3,
+       'n':3,
+       'p':1,
+       'k':0}
+arr = []
+for i in range(8):
+    name = list(input())
+    for k in range(8):
+        arr.append(name[k])
+    
+
+count_w = 0
+count_b = 0
+for el in arr:
+    if el in dct_w.keys():
+        count_w += dct_w[el]
+    elif el in dct_b.keys():
+        count_b += dct_b[el]
+        
+if count_w > count_b:
+    print ('White')
+elif count_w == count_b:
+    print ('Draw')
+else:
+    print('Black')
