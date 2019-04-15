3f1b8b337d172eb37f8856f9e27709e111d78373
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-04-15 16:15:54 +0300

    Create 104A-BlackJack.py

---
+++ 104A-BlackJack.py
@@ -0,0 +1,12 @@
+# https://codeforces.com/problemset/problem/104/A
+# https://codeforces.com/contest/104/submission/52778665
+
+
+n = int(input())
+n -= 10
+if (n < 10 or n == 11) and n >= 1:
+    print (4)
+elif n < 1 or n > 11:
+    print (0)
+else:
+    print (15)
